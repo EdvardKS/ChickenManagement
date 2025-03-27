e6c73621ce7789db70f70a5273c41cd20ab5638d
Edvard Khacahtryan Sahakyan <118193785+EdvardKS@users.noreply.github.com>
2025-03-27 13:28:48 +0000

    fix port

--- server/index.ts
+++ server/index.ts
@@ -115,7 +115,9 @@ app.use((req, res, next) => {
 
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
-  const port = 5000;
+  const env = process.env.NODE_ENV || "development";
+  const port = env === "production" ? 2025 : 5000;
+
   server.listen({
     port,
     host: "0.0.0.0",
